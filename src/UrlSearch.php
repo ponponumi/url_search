380b9d0fe04b5a0c1a795ef06816ecef32746d3c
ponpon <ponponumi@gmail.com>
2024-03-21 19:58:46 +0900

    URLを抽出するメソッドを作成

--- src/UrlSearch.php
+++ src/UrlSearch.php
@@ -3,5 +3,10 @@
 namespace Ponponumi\UrlSearch;
 
 class UrlSearch{
-  //
+  public static function search(string $text){
+    // URLを抽出する
+    preg_match_all('/(https?:\/\/(?:[a-zA-Z0-9_\-\.]+)(?:\:[0-9]+)?(?:[^\s]+))/', $text, $match_list);
+    $url_list = $match_list[0];
+    return $url_list;
+  }
 }
