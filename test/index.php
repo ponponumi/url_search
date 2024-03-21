dd16f45ea9567d4363ba66376c8d35718deb1281
ponpon <ponponumi@gmail.com>
2024-03-21 20:00:55 +0900

    テキストを作成

--- test/index.php
+++ test/index.php
@@ -1,3 +1,9 @@
 <?php require __DIR__ . "/header.php" ?>
 
+<?php
+
+$text = "Google検索はこちら https://www.google.co.jp/ Yahoo検索はこちら https://www.yahoo.co.jp/";
+
+?>
+
 <?php require __DIR__ . "/footer.php" ?>
