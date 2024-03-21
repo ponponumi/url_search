aa1ff72b83a27a73d57bb1a1236842c6314a007d
ponpon <ponponumi@gmail.com>
2024-03-21 20:08:01 +0900

    URLがない場合のテストを追加

--- test/not.php
+++ test/not.php
@@ -1,3 +1,17 @@
 <?php require __DIR__ . "/header.php" ?>
 
+<?php
+
+$text = "URLはありません";
+
+$list = \Ponponumi\UrlSearch\UrlSearch::search($text);
+$list_pos = \Ponponumi\UrlSearch\UrlSearch::searchPos($text);
+
+?>
+
+<p><?= $text ?></p>
+
+<pre><?php var_dump($list) ?></pre>
+<pre><?php var_dump($list_pos) ?></pre>
+
 <?php require __DIR__ . "/footer.php" ?>
