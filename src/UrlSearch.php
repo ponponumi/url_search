99f5d659df21cd5f10efeb16be98a9f5d1c5861d
ponpon <ponponumi@gmail.com>
2024-03-21 20:05:01 +0900

    URLの開始位置も返すメソッドを作成

--- src/UrlSearch.php
+++ src/UrlSearch.php
@@ -9,4 +9,30 @@ class UrlSearch{
     $url_list = $match_list[0];
     return $url_list;
   }
+
+  public static function searchPos(string $text){
+    // URLの開始位置も返す
+    $url_list = self::search($text);
+    $result = [];
+
+    if($url_list !== []){
+      // URLがあれば
+      $start = 0;
+
+      foreach ($url_list as $url) {
+        $pos = mb_strpos($text,$url,$start);
+
+        if($pos !== false){
+          $start = $pos + 1;
+        }
+
+        $result[] = [
+          "pos" => $pos,
+          "value" => $url,
+        ];
+      }
+    }
+
+    return $result;
+  }
 }
