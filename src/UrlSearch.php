91e151f542a3b03429322b5bfd42631297e01403
ponpon <ponponumi@gmail.com>
2024-03-23 01:57:09 +0900

    パターンをメソッドから取得するように変更

--- src/UrlSearch.php
+++ src/UrlSearch.php
@@ -9,7 +9,8 @@ class UrlSearch{
 
   public static function search(string $text){
     // URLを抽出する
-    preg_match_all('/(https?:\/\/(?:[a-zA-Z0-9_\-\.]+)(?:\:[0-9]+)?(?:[^\s]+))/', $text, $match_list);
+    $pattern = self::patternGet();
+    preg_match_all($pattern, $text, $match_list);
     $url_list = $match_list[0];
     return $url_list;
   }
